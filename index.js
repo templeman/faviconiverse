c9a3c05065e8981fa6c75ec18e82239c391f8928
Sam Templeman <samuel.templeman@me.com>
2012-10-09 13:21:23 -0700

    Test default favicon

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express')
+	//, nano = require('nano')('http://localhost:5984')
   , nano    = require('nano')(process.env.CLOUDANT_URL)
   // , app     = module.exports = express.createServer()
   , db_name = "test"
@@ -7,6 +8,8 @@ var express = require('express')
 var app = express.createServer(express.logger());
 var per_page = 10
 	, params = {include_docs: true, descending: true};
+
+app.use(express.favicon());
 app.get("/", function(request, response) {
 	db.attachment.get('pendletonpsych.com', 'favicon.ico').pipe(response);
 //	db.list(params, function(err, body, headers) {
