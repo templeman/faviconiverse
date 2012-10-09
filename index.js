567a703615d303125cd8d17e6dd1c49bd6ff14e9
Sam Templeman <samuel.templeman@me.com>
2012-10-08 23:00:07 -0700

    Test for Express server functionality

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require('express')
   , nano    = require('nano')(process.env.CLOUDANT_URL)
-  , app     = module.exports = express.createServer()
+  // , app     = module.exports = express.createServer()
   , db_name = "test"
   , db      = nano.use(db_name);
 
+var app = express.createServer(express.logger());
 var per_page = 10
 	, params = {include_docs: true, descending: true};
-app.get("/", function(request,response) {
+app.get("/", function(request, response) {
 	db.attachment.get('zj.com', 'favicon.ico').pipe(response);
 //	db.list(params, function(err, body, headers) {
 //		console.log(body);
 	//});
 });
 
-app.listen(3333);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	console.log("listening on " + port);
+});
