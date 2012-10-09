2bfb0f047e56ab4c8478258d0594209431feec85
Sam Templeman <samuel.templeman@me.com>
2012-10-08 22:18:42 -0700

    Testing nano.use(db_name)

--- master.js
+++ master.js
@@ -6,7 +6,7 @@ var dns = require("dns")
 
 
 var db_name = process.env.CLOUDANT_URL;
-var db = nano.use(db_name);
+var db = nano.use('"'+db_name+'"');
 // unreserved uri characters = ALPHA / DIGIT / "-" / "." / "_" / "~"
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
